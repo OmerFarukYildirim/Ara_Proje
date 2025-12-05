9ab5997d45dd5e7eca410ee8e36e066e58a9883e
OmerFarukYildirim <omeeryldrmm01@gmail.com>
2025-12-05 08:19:30 +0300

    Scheduled added to fetcher service for otomatically create news

--- FetcherMicroService/src/main/java/com/FetcherMicroService/dtos/KafkaCategoryRequest.java
+++ FetcherMicroService/src/main/java/com/FetcherMicroService/dtos/KafkaCategoryRequest.java
@@ -1,9 +1,13 @@
 package com.FetcherMicroService.dtos;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true) // JSON'da bilmediğin alan varsa hata verme
 public class KafkaCategoryRequest {
     // Python'un yolladığı JSON'daki 'category' anahtarıyla eşleşir
